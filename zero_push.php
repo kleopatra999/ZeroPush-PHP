7674d2e7bc85aedbeaf593c9b192a926f802e52c
Stefan Natchev <stefan.natchev@gmail.com>
2014-01-11 01:48:40 -0500

    fix response parsing

--- zero_push.php
+++ zero_push.php
@@ -18,68 +18,75 @@
 class ZeroPush
 {
   const VERSION = '0.0.1';
-  const USER_AGENT = "ZeroPush-PHP ({$VERSION})";
+  const USER_AGENT = "ZeroPush-PHP ({self::VERSION})";
   const URL = 'https://api.zeropush.com';
 
 	protected $settings = array();
 
-	public function __construct($settings = array()) {
+	public function __construct($auth_token) {
+    $settings = array('auth_token' => $auth_token);
 		$this->settings = array_merge($this->settings, $settings);
 	}
 
   public function verifyCredentials() {
-    request('get', '/verify_credentials');
+    return $this->request('get', '/verify_credentials');
   }
 
   public function notify($params) {
-    request('post', '/notify', $params);
+    return $this->request('post', '/notify', $params);
   }
 
   public function broadcast($params) {
-    request('post', '/broadcast', $params)
+    return $this->request('post', '/broadcast', $params);
   }
 
   public function register($params) {
-    request('post', '/register', $params);
+    return $this->request('post', '/register', $params);
   }
 
   public function unregister($params) {
-    request('delete', '/register', $params);
+    return $this->request('delete', '/register', $params);
   }
 
   public function subscribe($params) {
-    request('post', '/subscribe');
+    return $this->request('post', '/subscribe');
   }
 
-  public function subscribe($params) {
-    request('delete', '/subscribe');
+  public function unsubscribe($params) {
+    return $this->request('delete', '/subscribe');
   }
 
   public function setBadge($params) {
-    request('post', '/set_badge', $params);
+    return $this->request('post', '/set_badge', $params);
   }
 
   public function inactiveTokens() {
-    request('get', '/inactive_tokens');
+    return $this->request('get', '/inactive_tokens');
   }
 
   //
   private function request($verb, $path, $params = array()) {
     $headers = array();
-    $headers[]= "User-Agent: {$USER_AGENT}";
+    $headers[]= "User-Agent: {self::USER_AGENT}";
 
-    $curl = curl_init(URL . $path);
+    $curl = curl_init(self::URL . $path);
     curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
     curl_setopt($curl, CURLOPT_CUSTOMREQUEST, strtoupper($verb));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-    $params = array_merge($params, ['auth_token', $this->settings['auth_token']);
+    $params = array_merge($params, $this->settings);
     curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
 
     $response = curl_exec($curl);
+
+    if($response == null) {
+      error_log(curl_error($curl));
+    }
+
     curl_close($curl);
 
     //parse
-    $json = json_decode($response);
+    $json = json_decode($response, true);
 
     return $json;
   }
